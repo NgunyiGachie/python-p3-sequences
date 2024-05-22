4ed4fedce201535b1bd739e4b53142fe1f33f3e0
NgunyiGachie <antogachie@gmail.com>
2024-05-22 13:29:36 +0300

    Complete assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+fib_sequence = [0, 1]
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        while len(fib_sequence) < length:
+            next_fib = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_fib)
+            
+        print(fib_sequence)
